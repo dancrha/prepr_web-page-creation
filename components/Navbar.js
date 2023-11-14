79b6bdb3207a00e6866b0c69e6b6b90ca2f55d9f
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-11-14 18:13:48 -0500

    navbar in

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,27 +1,26 @@
 import React from "react"; // Don't forget to import React
 import { navLinks } from "../constants";
 
-const Nav = () => {
+const Navbar = () => {
   return (
     <header className='padding-x py-8 absolute z-10 w-full'>
       <nav className='flex justify-between items-center max-container'>
-        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+        <ul className='flex-1 flex justify-center items-center gap-16'>
           {/* Map over the navLinks array */}
           {navLinks.map((item) => (
             <li key={item.label}>
               <a
                 href={item.href}
-                className='font-montserrat leading-normal text-lg text-slate-gray'
+                className='font-montserrat font-semibold leading-normal text-lg text-slate-gray'
               >
                 {item.label}
               </a>
             </li>
           ))}
         </ul>
-        <div className='hidden max-lg:block'></div>
       </nav>
     </header>
   );
 };
 
-export default Nav;
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -5,40 +5,42 @@ import Link from "next/link";
 import Navbar from "@/components/Navbar";
 
 const LandingHero = () => {
-  <Navbar />;
   return (
-    <div className='text-white font-bold py-36 text-center space-y-5'>
-      <div className='text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold'>
-        <h1 className='text-gray-700'>Explore Your Interests</h1>
-        <div className='text-transparent bg-clip-text bg-gradient-to-r pb-5 from-blue-500  to-green-500'>
-          <TypewriterComponent
-            options={{
-              strings: [
-                "Engaging Labs.",
-                "Fun Challenges.",
-                "Exciting Projects.",
-              ],
-              autoStart: true,
-              loop: true,
-            }}
-          />
+    <div>
+      <Navbar />
+      <div className='text-gray-700 font-bold py-36 text-center space-y-5'>
+        <div className='text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold'>
+          <h1 className='text-gray-700'>Explore Your Interests</h1>
+          <div className='text-transparent bg-clip-text bg-gradient-to-r pb-5 from-blue-500  to-green-500'>
+            <TypewriterComponent
+              options={{
+                strings: [
+                  "Engaging Labs.",
+                  "Fun Challenges.",
+                  "Exciting Projects.",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </div>
         </div>
-      </div>
-      <div className='text-sm md:text-xl font-light text-zinc-400'>
-        Get rewarding hands-on experience.
-      </div>
-      <div>
-        <Link href={"/first-post"}>
-          <button
-            variant='premium'
-            className='md:text-lg p-4 md:p-6 rounded-full font-semibold'
-          >
-            Start Generating for free
-          </button>
-        </Link>
-      </div>
-      <div className='text-zinc-400 text-xs md:Ltext-sm font-normal'>
-        No credit card required.
+        <div className='text-sm md:text-xl font-light text-zinc-400'>
+          Get rewarding hands-on experience.
+        </div>
+        <div>
+          <Link href={"/first-post"}>
+            <button
+              variant='premium'
+              className='md:text-lg p-4 md:p-6 rounded-full font-semibold'
+            >
+              Start Generating for free
+            </button>
+          </Link>
+        </div>
+        <div className='text-zinc-400 text-xs md:Ltext-sm font-normal'>
+          No credit card required.
+        </div>
       </div>
     </div>
   );
