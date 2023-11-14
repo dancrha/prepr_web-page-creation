a5ff1810844dff73d99002652b70da0779c8445b
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-11-14 18:57:19 -0500

    navbar almost done

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,21 @@
 import React from "react"; // Don't forget to import React
 import { navLinks } from "../constants";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <header className='padding-x py-8 absolute z-10 w-full'>
-      <nav className='flex justify-between items-center max-container'>
-        <ul className='flex-1 flex justify-center items-center gap-16'>
+    <header className='py-4 px-8 sticky z-10 w-full'>
+      <nav className='flex max-container items-center'>
+        <Image
+          src='/assets/logo.png'
+          width={130}
+          height={130}
+          alt='preprlabs logo'
+        />
+
+        <ul className='pl-10 flex mx-auto pt-3 gap-16'>
+          <div className='pl-10'></div>
+          <p className=''></p>
           {/* Map over the navLinks array */}
           {navLinks.map((item) => (
             <li key={item.label}>
@@ -18,6 +28,28 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+        <ul className='flex gap-4 '>
+          <Image
+            src='/assets/notification.png'
+            width={30}
+            height={30}
+            alt='notification'
+          />
+          <Image
+            src='/assets/message.png'
+            width={30}
+            height={30}
+            alt='message'
+          />
+          <Image src='/assets/help.png' width={30} height={30} alt='help' />
+        </ul>
+        <li className='list-none pl-4'>
+          <input
+            type='text'
+            placeholder='Search...'
+            className='border border-gray-400 px-2 py-1 rounded'
+          />
+        </li>
       </nav>
     </header>
   );
