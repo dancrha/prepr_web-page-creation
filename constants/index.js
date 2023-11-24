a87c026a8838776826624b8aef59050c2a100dea
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-11-23 20:57:25 -0500

    change

--- constants/index.js
+++ constants/index.js
@@ -1,7 +1,7 @@
 export const navLinks = [
   { href: "/.", label: "Home" },
+  { href: "/dashboard", label: "Dashboard" },
   { href: "/labs", label: "Labs" },
   { href: "/challenges", label: "Challenges" },
   { href: "/projects", label: "Projects" },
-  { href: "/dashboard", label: "Dashboard" },
 ];
